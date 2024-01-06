47838d178eaa3ded2b9cb813c7d18811beab83d1
Matheus Westphal Lima <matheuswestphal07@gmail.com>
2024-01-06 18:09:41 -0300

    Subindo aulas adicionais

---
+++ Modulo01/Aula08.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro numero:"))
+n2 = float(input("Digite o segundo numero"))
+
+soma = n1 + n2
+print(f"A primeira variavel é do tipo {type(n1)}\n e o valor da primeira variavel é {n1}")
+print(f"A segunda variavel é do tipo {type(n2)}\n e o valor da segunda variavel é {n2}")
+
+print(f"O resultado final é do tipo primitivo{type(soma)}\n e o valor do resultado") #diz o tipo da soma (float)
---
+++ Modulo01/Aula09.py
@@ -0,0 +1,6 @@
+pera = input("Digite qualquer coisa:")
+
+print("tem espaço", pera.isspace())
+print("tem caracter numerico", pera.isnumeric())
+print("contém caracter alfabetico", pera.isalpha)
+print("contem caracter alfanumerico", pera.isalnum)
